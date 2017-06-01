a2fbfaec0371baf89e532493beab90c353083812
Ramana C S <srchaitanya@gmail.com>
2017-06-01 09:41:25 +0530

    profile panel related changes

--- imports/api/profile/methods.js
+++ imports/api/profile/methods.js
@@ -1,17 +1,28 @@
 import {Profile} from './userProfile.js';
 import {Meteor} from 'meteor/meteor';
 
+if(Meteor.isServer) {
+	Meteor.publish('MyProfile',function profileManagement(){
+		var userId = this.userId;
+		console.log("in publishing..."+userId);
+		var cursor = Profile.find({_id:userId});
+		console.log("on server>>>"+cursor.count());
+		this.ready();
+		return cursor;
+	});
+}
 
 Meteor.methods({
 	'profile.upsert':function(profileObj){
-		console.log("inside profile.upsert");
 		var fname = profileObj.fname;
 		var lname = profileObj.lname;
 		var sex   = profileObj.sex;
 		var address= profileObj.address;
-		var _id    = profileObj._id;
-		var cursor = Profile.insert({'_id':_id, 'fname':fname,'lname':lname,'sex':sex,'address':address});
-		console.log("profile inserted:"+cursor);
+		var id    = Meteor.userId();
+		console.log("fname:"+fname+";lname:"+lname+";gender:"+sex+";address:"+address);
+		var cursor = Profile.update({'_id':id},{$set:{'fname':fname,'lname':lname,'sex':sex,'address':address}});
+		console.log("profile.upsert:update:"+cursor);
 		return cursor;
 	}
 });
+
--- imports/ui/pages/profile/manageProfile.js
+++ imports/ui/pages/profile/manageProfile.js
@@ -1,18 +1,33 @@
-import { Router } from 'meteor/iron:router';
-import {Meteor} from 'meteor/meteor';
+import {Router}  from 'meteor/iron:router';
+import {Meteor}  from 'meteor/meteor';
+import {ReactiveDict} from 'meteor/reactive-dict';
 import {Profile} from '../../../api/profile/userProfile.js';
-import                '../../../api/profile/methods.js';
-
+import '../../../api/profile/methods.js';
 import './manageProfile.html';
 
+Template.body.onCreated(function onCreate(){
+	this.state = new ReactiveDict();
+	Meteor.subscribe('MyProfile');
+});
+
 Template.manageProfile.helpers({
+	gender:function(){
+		return [{key:"M",value:"Male"},{key:"F",value:"Female"},{key:"O",value:"Others"}];
+	},
 	userId:function(){
 		return Meteor.userId();
 	},
 	profileData:function(){
-		var userId = Meteor.userId();
-		var cursor = Profile.find({_id:userId});
-		return cursor;
+		var cursor = Profile.find({});
+		console.log("on client>>>"+cursor.count());
+		var profileObj = {};
+		cursor.forEach((profile) => {
+		  profileObj.fname = profile.fname;
+		  profileObj.lname = profile.lname;
+		  profileObj.sex   = profile.sex;
+		  profileObj.address = profile.address;
+		});
+		return profileObj;
 	}
 });
 
@@ -26,13 +41,12 @@ Template.manageProfile.events({
 	'submit form': function(event) {
 		event.preventDefault();
 		console.log("save clicked, user id:"+Meteor.userId);
-		var userId = Meteor.userId();
 		var firstName = event.target.firstName.value;
 		var lastName  = event.target.lastName.value;
 		var sex       = event.target.sex.value;
 		var address   = event.target.address.value;
 
-		var profileObj = {'_id':userId,'fname':firstName,'lname':lastName,'sex':sex,'address':address};
+		var profileObj = {'fname':firstName,'lname':lastName,'sex':sex,'address':address};
 
 		var result = Meteor.call('profile.upsert',profileObj);
 		
